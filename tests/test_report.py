27a3016ba50b76653300263a114066a25ea518c8
Lucas <lucas003simoes@gmail.com>
2019-10-15 01:54:59 -0300

    finished fixing all tests

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,3 +1,4 @@
+from arche import Arche
 from arche import SH_URL
 from arche.report import Report
 from arche.rules.result import Level
@@ -61,66 +62,18 @@ def test_write_rule_details(mocker, get_df, capsys, message, expected_details):
     assert generated_html.count("very detailed message") == 1
 
 
-@pytest.mark.parametrize(
-    "errors, short, keys_limit, expected_messages",
-    [
-        (
-            {"something happened": pd.Series(["1", "2"])},
-            False,
-            10,
-            ["2 items affected - something happened: 1, 2"],
-        ),
-        (
-            {"something went bad": [i for i in range(10)]},
-            False,
-            1,
-            ["10 items affected - something went bad: 5"],
-        ),
-        (
-            {
-                "err1": [i for i in range(15)],
-                "err2": [i for i in range(15)],
-                "err3": [i for i in range(15)],
-                "err4": [i for i in range(15)],
-                "err5": [i for i in range(15)],
-                "err6": [i for i in range(15)],
-            },
-            False,
-            1,
-            [f"15 items affected - err{i}: 5" for i in range(1, 7)],
-        ),
-        (
-            {
-                "err1": ["1"],
-                "err2": ["2"],
-                "err3": ["3", "4"],
-                "err4": ["5", "6", "7"],
-                "err5": ["7", "8"],
-            },
-            True,
-            1,
-            [
-                "3 items affected - err4: 5, 6, 7",
-                "2 items affected - err3: 3, 4",
-                "2 items affected - err5: 7, 8",
-                "1 items affected - err1: 1",
-                "1 items affected - err2: 2",
-            ],
-        ),
-        (
-            {"something happened": set(["https://app.scrapinghub.com/p/1/1/1/item/0"])},
-            False,
-            10,
-            [f"1 items affected - something happened: [0]({SH_URL}/1/1/1/item/0)"],
-        ),
-        ({"html '</br>'": [1]}, False, 10, ["1 items affected - html '</br>': 1"]),
-    ],
-)
-def test_write_detailed_errors(mocker, errors, short, keys_limit, expected_messages):
-    mocker.patch("pandas.Series.sample", return_value=pd.Series("5"), autospec=True)
-    md_mock = mocker.patch("arche.report.display_markdown", autospec=True)
-    Report.write_detailed_errors(errors, short, keys_limit)
-    md_mock.assert_has_calls(mocker.call(m) for m in expected_messages)
+def test_display_errors(mocker, get_job_items, get_schema):
+    mocked_display = mocker.patch("arche.report.display_html", autospec=True)
+    url = f"{SH_URL}/112358/13/21/item/1"
+    schema = {"type": "object", "required": ["price"], "properties": {"price": {}}}
+    g = Arche("source", schema=schema)
+    g._source_items = get_job_items
+    g.report_all()
+    generated_html = mocked_display.mock_calls[0][1][0]
+    mocked_display.assert_called_once()
+    assert "JSON Schema Validation - FAILED" in mocked_display.mock_calls[0][1][0]
+    assert generated_html.count('href="{}"'.format(url)) == 1
+    assert generated_html.count("&#39;price&#39; is a required property") == 1
 
 
 @pytest.mark.parametrize(
